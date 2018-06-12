a482d02308e74aa8acf82bf82c9edd591e0bca0a
ren096358 <ren096358@gmail.com>
2018-06-12 17:22:36 +0800

    * add config.php * fix bug for sql

--- Model.php
+++ Model.php
@@ -13,10 +13,10 @@ abstract class Model
     public function init()
     {
         try {
-            $db_host = 'localhost';
-            $db_name = '';
-            $db_user = 'root';
-            $db_password = '';
+            $db_host = DB_HOST;
+            $db_name = DB_NAME;
+            $db_user = DB_USER;
+            $db_password = DB_PASSWD;
             $dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8";
             $this->conn = new PDO($dsn, $db_user, $db_password);
         } catch (Exception $e) {
--- models/IndexModel.php
+++ models/IndexModel.php
@@ -12,11 +12,11 @@ class IndexModel extends Model
         $this->table = "";
     }
 
-    public function get()
+    public function get($id)
     {
         try {
             $stmt = $this->conn->prepare("SELECT * FROM $this->table WHERE id = :id");
-
+            $stmt->bindParam(':id', $id);
             $stmt->execute();
             return $stmt->fetch();
         } catch (PDOException $e) {
@@ -27,7 +27,9 @@ class IndexModel extends Model
     public function insert($data)
     {
         try {
-            $stmt = $this->conn->prepare("SELECT * FROM $this->table WHERE used = 0 ORDER BY RAND() LIMIT 1");
+            $stmt = $this->conn->prepare("INSERT INTO $this->table (name, email) VALUES (:name, :email)");
+            $stmt->bindParam(':name', $data->name);
+            $stmt->bindParam(':email', $data->email);
             return $stmt->execute();
         } catch (PDOException $e) {
             echo "Error: " . $e->getMessage();
@@ -37,7 +39,10 @@ class IndexModel extends Model
     public function update($id, $data)
     {
         try {
-            $stmt = $this->conn->prepare("SELECT * FROM $this->table WHERE used = 0 ORDER BY RAND() LIMIT 1");
+            $stmt = $this->conn->prepare("UPDATE $this->table SET name = :name, email = :email WHERE id = :id");
+            $stmt->bindParam(':name', $data->name);
+            $stmt->bindParam(':email', $data->email);
+            $stmt->bindParam(':id', $id);
             $stmt->execute();
             return $stmt->fetch();
         } catch (PDOException $e) {
@@ -48,7 +53,8 @@ class IndexModel extends Model
     public function delete($id)
     {
         try {
-            $stmt = $this->conn->prepare("SELECT * FROM $this->table WHERE used = 0 ORDER BY RAND() LIMIT 1");
+            $stmt = $this->conn->prepare("DELETE FROM $this->table WHERE id = :id");
+            $stmt->bindParam(':id', $id);
             $stmt->execute();
             return $stmt->fetch();
         } catch (PDOException $e) {
